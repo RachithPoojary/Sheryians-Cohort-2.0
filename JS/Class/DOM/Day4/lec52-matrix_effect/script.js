93bffe66f9e26419c1d82490284e0c78005bcf33
Rachith Poojary <rachithstudy@gmail.com>
2026-06-15 23:56:14 +0530

    matrix effect

--- JS/Class/DOM/Day4/lec52-matrix_effect/script.js
+++ JS/Class/DOM/Day4/lec52-matrix_effect/script.js
@@ -1,14 +1,32 @@
 let  para = document.querySelector('p');
 let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 let text = para.innerHTML;
+let iteration = 0;
 
-para.addEventListener('mouseenter',()=>{
+// para.addEventListener('mouseenter',()=>{
 
-    setInterval(()=>{
-        const str = text.split('').map(()=>{
-            return characters.split('')[Math.floor(Math.random()*53)];
-        }).join('');
-        para.innerHTML = str;
+//     setInterval(()=>{
+//         const str = text.split('').map((char,idx)=>{
+//             if(idx<iteration){
+//                 return char;
+//             }
+//             return characters.split('')[Math.floor(Math.random()*53)];
+//         }).join('');
+//         para.innerHTML = str;
+//         iteration += 0.5;
 
-    },30);
-})
+//     },30);
+// })
+
+
+function randomText(){
+    const str = text.split('').map((char,idx)=>{
+        if(idx<iteration) return char
+        return characters.split('')[Math.floor(Math.random()*52)];
+    }).join('');
+
+    para.innerHTML = str;
+    iteration += 0.25;
+}
+
+setInterval(randomText,30);
